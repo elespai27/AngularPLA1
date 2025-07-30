ce74ada70ec791bd1e3a1f3373d3bc6c83f037ab
elespai27 <elespai27@gmail.com>
2025-07-30 13:37:15 +0200

    Adding reactive variable You are in: page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, NavigationEnd, Router } from '@angular/router';
 import { Header } from './components/header/header';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,21 @@ import { Header } from './components/header/header';
 })
 export class App {
   protected title = 'the-little-archive';
+  currentPage: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      if (event.urlAfterRedirects.includes('login')) {
+        this.currentPage = 'Login';
+      } else if (event.urlAfterRedirects.includes('resources')) {
+        this.currentPage = 'Resources';
+      } else if (event.urlAfterRedirects.includes('about')) {
+        this.currentPage = 'About';
+      } else {
+        this.currentPage = '';
+      }
+    });
+  }
 }
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  @Input() page: string = '';
 
 }
